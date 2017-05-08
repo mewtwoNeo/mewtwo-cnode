ae0c50d6a06d1e21316cd5f25a9ce391a4168f41
mewtwo-chi <ffchixiang@gmail.com>
2017-05-08 19:31:50 +0800

    修改结构

--- src/api/api.js
+++ src/api/api.js
@@ -2,21 +2,11 @@
  * Created by dengchongjing on 2017/3/2.
  */
 import axios from 'axios'
-
-// 域名
-const HOST = 'https://cnodejs.org/api/v1'
+import API_TYPE from './config'
 
 // 如果需要代理，加上这层路径
 // const proxy = '/proxy'
 
-export const API_TYPE = {
-  topic: {
-    // 主题首页
-    get_topics: '/topics',
-    // 主题详情
-    get_topic_id: '/topic'
-  }
-}
 
 // get方法封装
 export function get (url) {
@@ -26,10 +16,10 @@ export function get (url) {
 
 // 获取主题首页
 export function getTopics (page = 1, tab = 'all', limit = 10, mdrender = true) {
-  return get(`${API_TYPE.topic.get_topics}?page=${page}&tab=${tab}&limit=${limit}&mdrender=${mdrender}`)
+  return get(`${API_TYPE.get_topics}?page=${page}&tab=${tab}&limit=${limit}&mdrender=${mdrender}`)
 }
 
 // 获取主题详情
 export function getTopicId (id, mdrender = true) {
-  return get(`${API_TYPE.topic.get_topic_id}/${id}?mdrender=${mdrender}`)
+  return get(`${API_TYPE.get_topic_id}/${id}?mdrender=${mdrender}`)
 }
--- src/api/config.js
+++ src/api/config.js
@@ -4,6 +4,10 @@ const HOST = 'https://cnodejs.org/api/v1'
 export default {
   // 主题首页
   get_topics: `${HOST}/topics`,
+  // 主题详情
   get_topic: `${HOST}/topic`,
-  post_topics: `${HOST}/topics`
+  // 新建主题
+  post_topics: `${HOST}/topics`,
+  // 编辑主题
+  post_topics_update: `${HOST}/topics/update`
 }
